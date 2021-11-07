69bec05e448f88a4304c5e693006368748e0e30e
Hasibur Rahman <hasib.active@gmail.com>
2021-11-07 03:48:20 -0800

    init layout

---
+++ layouts/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Sidebar</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex">
+        <!-- Sidebar -->
+        <sidebar></sidebar>
+
+        <!-- Main Content -->
+        <main class="flex-grow">
+            <Nuxt />
+        </main>
+    </div>
+</template>
+
+<script>
+import Sidebar from './Sidebar.vue'
+export default {
+    components: {
+        Sidebar,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
